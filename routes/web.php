ef8377c91437dcdb00711e172720877060e955a0
Chetan Singh <chetan.singh@webeesocial.com>
2024-08-14 12:22:57 +0530

    iniital commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\DashboardController;
+
+Route::get('/', function () {
+    return 'Welcome to acma fontend';
+});
+
+Route::get('/admin/login', [AuthController::class, 'login'])->name('admin.login');
+Route::post('/admin/login', [AuthController::class, 'authenticate'])->name('admin.authenticate');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('/dashboard', [DashboardController::class,'index'])->name('admin.dashboard');
+
+    Route::get('/logout', [AuthController::class, 'logout'])->name('admin.logout');
+
+});
